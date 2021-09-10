3a81ba808c322a812da470d8650c2be2d6c284b2
ortariot <ortariot@gmail.com>
2021-09-10 20:45:45 +0700

    errors

--- app/views.py
+++ app/views.py
@@ -1,25 +1,33 @@
+from django.core import paginator
 from app.settings import BUS_STATION_CSV
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
 import csv
 
 
 def index(request):
-    return redirect(reverse(bus_stations))
+    return redirect(reverse(bus_stations, args=(1,)))
 
 
-def bus_stations(request):
+def bus_stations(request, page):
+
     path = BUS_STATION_CSV
     with open(path, encoding='cp1251') as file:
         reader = csv.DictReader(file)
         data = list(reader)
+  
+    print(page)
+    
+    paginate = Paginator(data, 10)
 
-    current_page = 1
-    next_page_url = 'write your url'
+    data_list = paginate.get_page(page)
+    next_page_url = f'/bus_stations?page={page + 1}'
+    prev_page_url = f'/bus_stations?page={page - 1}'
     return render(request, 'index.html', context={
-        'bus_stations': data,
-        'current_page': current_page,
-        'prev_page_url': None,
+        'bus_stations': data_list,
+        'current_page': page,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
 
